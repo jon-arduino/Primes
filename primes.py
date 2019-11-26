b190fae709f978e37fe8867b135e54cd467c61c1
jon-arduino <jonlotz@gmail.com>
2019-11-25 21:57:47 -0800

    First working code

--- primes.py
+++ primes.py
@@ -1,2 +1,15 @@
-print("I have a dream")
+max_primes = 100
+print("Compute all primes less than", max_primes)
+prime_list=[]
+
+for number in range(2,max_primes):
+  its_a_prime=1
+  for prime in prime_list:
+      if (number % prime == 0):     # if number is not a prime
+          its_a_prime=0
+          break
+  if (its_a_prime):
+      prime_list = prime_list + [number]
+      print(number)
+
 
