cef9ec2f5dc1a4bd0012e0902237670da142943e
jon-arduino <jonlotz@gmail.com>
2019-11-25 22:30:44 -0800

    added cmd line option

--- primes.py
+++ primes.py
@@ -1,8 +1,14 @@
-max_primes = 100
-print("Compute all primes less than", max_primes)
+import sys
+
+max_prime = 100
+prime_count=0
+if len(sys.argv) >1:
+    max_prime = int(sys.argv[1])
+
+print("Count all primes less than", max_prime)
 prime_list=[]
 
-for number in range(2,max_primes):
+for number in range(2,max_prime):
   its_a_prime=1
   for prime in prime_list:
       if (number % prime == 0):     # if number is not a prime
@@ -10,6 +16,8 @@ for number in range(2,max_primes):
           break
   if (its_a_prime):
       prime_list = prime_list + [number]
-      print(number)
+      prime_count += 1
+      # print(number)
 
-
+print("total primes found less than",max_prime,"=",prime_count)
+#print("size of list = ",len(prime_list) )
